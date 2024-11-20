505f2666579a334a5c46551ccb94b2cba7d7181b
Artur Araujo <arturavs1996@gmail.com>
2024-11-20 12:58:51 -0300

    criado o banco de dados e a tabela users

--- src/models.py
+++ src/models.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
 
-
 @table_registry.mapped_as_dataclass
+
+
 class User:
     __tablename__ = 'users'
 
@@ -15,4 +16,3 @@ class User:
     email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(init=False, onupdate=func.now())
-    
